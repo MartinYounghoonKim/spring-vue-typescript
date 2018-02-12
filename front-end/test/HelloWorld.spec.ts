967174202061ae8d2b8872c512e30c0d05f54687
martinyounghoonkim <hoons0131@gmail.com>
2018-02-13 00:01:32 +0900

    Testing impor vue component

--- front-end/test/HelloWorld.spec.ts
+++ front-end/test/HelloWorld.spec.ts
@@ -1,4 +1,12 @@
+import { mount } from '@vue/test-utils';
+import HelloWorld from '../src/components/hello-world/HelloWorld.vue';
+
 describe("ClassComponent.test.js", () => {
+    let wrapper: any;
+    beforeEach(() => {
+        wrapper = mount(HelloWorld);
+    });
     it(".message = 'Hello world'", () => {
         expect(true).toEqual(true);
     });
+});
