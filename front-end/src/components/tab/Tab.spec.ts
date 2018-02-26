3e5b930307e7eb14a32bb5cb75d7359b518e880a
martinyounghoonkim <hoons0131@gmail.com>
2018-02-26 10:38:49 +0900

    Update Tab unit test's files

---
+++ front-end/src/components/tab/Tab.spec.ts
@@ -0,0 +1,30 @@
+///<reference path="../../../../node_modules/@types/jest/index.d.ts"/>
+// https://vue-test-utils.vuejs.org/kr/
+import { mount } from "vue-test-utils";
+import Tab from './Tab.vue';
+
+describe('[Tab]', function () {
+    let cmp: any;
+
+    beforeEach(() => {
+        cmp = mount(Tab);
+    });
+
+    it("Tab vue.js 인스턴스 확인", () => {
+        expect(cmp.isVueInstance()).toBeTruthy();
+    });
+
+    it("페이지 로딩완료 시 첫 번째 탭 활성화", () => {
+        expect(cmp.vm.currentTabIndex).toEqual(0);
+        expect(cmp.find('.tab-content').is('div')).toBe(true);
+    });
+
+    it("탭버튼 클릭 시 버튼 활성화 컨텐츠 레이어 활성화 변경", () => {
+        cmp
+            .findAll('button.tab-title')
+            .at(1)
+            .trigger('click');
+        expect(cmp.vm.currentTabIndex).toEqual(1);
+        expect(cmp.find('.tab-content').text()).toBe('tab2-content');
+    });
+});
