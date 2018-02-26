4bdef98445ba2bc448cfa2fc1256f6e5ddf92bee
martinyounghoonkim <hoons0131@gmail.com>
2018-02-27 00:14:37 +0900

    Code refactoring with tab indent

--- front-end/src/components/tab/Tab.vue
+++ front-end/src/components/tab/Tab.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
     <ul>
-      <li
-              v-for="(title, index) in tabs.map(tab => tab.title)"
-              v-bind:key="index">
-        <button
-                @click="changeCurrentTabIndex(index)"
-                v-bind:class="[
-            `tab-title`,
-            `tab-title-${index}`,
-            { 'tab-title-current' : index === currentTabIndex }
-          ]">
+      <li v-for="(title, index) in tabs.map(tab => tab.title)"
+          :key="index">
+        <button @click="changeCurrentTabIndex(index)"
+                :class="[
+                  `tab-title`,
+                  `tab-title-${index}`,
+                  { 'tab-title-current' : index === currentTabIndex }
+                ]">
           {{title}}
         </button>
       </li>
     </ul>
-    <div
-            class="tab-content"
-            v-if="index === currentTabIndex"
-            v-for="(content, index) in tabs.map(tab => tab.content)"
-            v-bind:key="index">
+    <div class="tab-content"
+         v-if="index === currentTabIndex"
+         v-for="(content, index) in tabs.map(tab => tab.content)"
+         :key="index">
       {{content}}
     </div>
-
   </div>
 </template>
 
@@ -36,6 +32,7 @@
             title: string;
             content: string;
         }>;
+
         changeCurrentTabIndex(index: number): void;
     }
 
@@ -43,10 +40,11 @@
     export default class TabSample extends Vue implements TabSampleInterface {
         currentTabIndex = 0;
         tabs = [
-            { title: "tab1-title", content: "tab1-content" },
-            { title: "tab2-title", content: "tab2-content" },
-            { title: "tab3-title", content: "tab3-content" }
+            {title: "tab1-title", content: "tab1-content"},
+            {title: "tab2-title", content: "tab2-content"},
+            {title: "tab3-title", content: "tab3-content"}
         ];
+
         changeCurrentTabIndex(index: number) {
             this.currentTabIndex = index;
         }
