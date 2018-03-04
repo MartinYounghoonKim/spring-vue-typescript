32c6dae6edb15d1ca81fcf168276841b25519b0d
martinyounghoonkim <hoons0131@gmail.com>
2018-03-04 21:26:28 +0900

    Add interfaces for Todo api

---
+++ front-end/src/Interfaces/Todo.Interface.ts
@@ -0,0 +1,13 @@
+export interface Todo {
+    id: string,
+    todo: string,
+    isDone?: boolean
+}
+
+export interface Event {
+    timestamp: number;
+}
+
+export interface MouseEvent extends Event {
+    target: HTMLElement;
+}
