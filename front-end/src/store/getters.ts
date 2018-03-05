b1c13ed4dc99db723408be078ad31fe2683bc3c1
martinyounghoonkim <hoons0131@gmail.com>
2018-03-05 21:13:01 +0900

    Add store for todo api's Fetch function

--- front-end/src/store/getters.ts
+++ front-end/src/store/getters.ts
@@ -2,11 +2,8 @@ import { GetterTree } from 'vuex'
 
 
 const todoGetters: GetterTree<any, any> = {
-    isAllChecked (state: any) {
-        return state.todos.every( (todo: any) => todo.isDone === true);
-    },
-    getTodosCounter (state: any) {
-        return state.todos.filter( (v: any) => v.isDone === true ).length;
+    getTodos (state) {
+        return state.todos;
     }
 };
 
