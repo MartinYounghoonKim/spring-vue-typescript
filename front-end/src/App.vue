e781f2866a0e9cde995fa1c8bc01f9fbfb0aa6c2
martinyounghoonkim <hoons0131@gmail.com>
2018-03-11 16:22:53 +0900

    Add todo components with router

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,21 +1,25 @@
 <template>
-<div id="app">
-  <ul class="gnb">
-    <li>
-      <a href="#/">index</a>
-    </li>
-    <li>
-      <a href="#/tabSample">tabSample</a>
-    </li>
-  </ul>
-  <router-view/>
-</div>
+    <div id="app">
+        <ul class="gnb">
+            <li>
+                <router-link :to="{ name: 'Hello' }">Index</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'TabSample' }">tabSample</router-link>
+            </li>
+            <li>
+                <router-link :to="{ name: 'Todo' }">Todo</router-link>
+            </li>
+        </ul>
+        <router-view/>
+    </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Component from "vue-class-component";
+    import Vue from "vue";
+    import Component from "vue-class-component";
 
-@Component({})
-export default class App extends Vue {}
+    @Component({})
+    export default class App extends Vue {
+    }
 </script>
--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -2,20 +2,26 @@ import Vue from "vue";
 import Router from "vue-router";
 import HelloWorld from "../components/hello-world/HelloWorld.vue";
 import TabSample from "../components/tab/Tab.vue";
+import Todo from "../components/todo/todo.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "Hello",
-      component: HelloWorld
-    },
-    {
-      path: "/tabSample",
-      name: "TabSample",
-      component: TabSample
-    }
-  ]
+    routes: [
+        {
+            path: "/",
+            name: "Hello",
+            component: HelloWorld
+        },
+        {
+            path: "/tabSample",
+            name: "TabSample",
+            component: TabSample
+        },
+        {
+            path: "/todo",
+            name: "Todo",
+            component: Todo
+        }
+    ]
 });
