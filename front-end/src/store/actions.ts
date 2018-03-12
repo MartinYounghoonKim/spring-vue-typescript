176158805e0a85a5f35470af087dbb23946c9bc5
martinyounghoonkim <hoons0131@gmail.com>
2018-03-12 23:33:30 +0900

    Update action's codes

--- front-end/src/store/actions.ts
+++ front-end/src/store/actions.ts
@@ -2,9 +2,13 @@ import TODO_TYPES from './mutationsType';
 import todoServices from '../services/todoServices';
 import { ActionContext } from 'vuex';
 
-export function fetchTodos(store: ActionContext<any, any>) {
+import { Todo } from './states';
+
+type TodoContext = ActionContext<Todo, any>;
+
+export function fetchTodos(context: TodoContext ) {
     return todoServices.fetchTodo()
         .then((res: any) => {
-            store.commit(TODO_TYPES.FETCH, res.data);
-        })
+            context.commit(TODO_TYPES.FETCH, res.data);
+        });
 }
