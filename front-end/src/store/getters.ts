423a7a1fba633f8319e24f19d3dfd30c1f5f7be3
martinyounghoonkim <hoons0131@gmail.com>
2018-03-17 15:11:25 +0900

    Add state's type in getTodos function's parameter

--- front-end/src/store/getters.ts
+++ front-end/src/store/getters.ts
@@ -1,8 +1,9 @@
 import { GetterTree } from 'vuex'
 
+import { TodoState } from './states';
 
 const todoGetters: GetterTree<any, any> = {
-    getTodos (state) {
+    getTodos (state: TodoState) {
         return state.todos;
     }
 };
