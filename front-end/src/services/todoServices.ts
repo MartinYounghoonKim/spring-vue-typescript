2c634fe241ece8b181616a8f0cdadf396da851f4
martinyounghoonkim <hoons0131@gmail.com>
2018-03-21 12:33:41 +0900

    Add Mock data for fetch todo's data

--- front-end/src/services/todoServices.ts
+++ front-end/src/services/todoServices.ts
@@ -1,7 +1,22 @@
 import { API } from './coreServices';
 
 export function fetchTodo(): Promise<object> {
-    return API.get('/api/todos');
+    return new Promise(resolve => {
+        resolve({
+            data: [
+                {
+                    id: 1,
+                    todo: 'Todo List01',
+                    completed: false,
+                },{
+                    id: 2,
+                    todo: 'Todo List02',
+                    completed: true,
+                }
+            ]
+        });
+    });
+    // return API.get('/api/todos');
 }
 
 export default {
