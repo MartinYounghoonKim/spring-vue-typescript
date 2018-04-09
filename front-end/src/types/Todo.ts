02828bbcfb9ef04334417fe614ace5b486c9d658
martinyounghoonkim <hoons0131@gmail.com>
2018-04-09 22:38:30 +0900

    Update Todo's interface to using each situation

--- front-end/src/types/Todo.ts
+++ front-end/src/types/Todo.ts
@@ -7,3 +7,18 @@ export interface Todo {
 export interface TodoState {
     todos: Array<Todo>
 }
+
+export interface AddedTodo {
+    text: string
+}
+
+export interface FetchedTodo {
+    id: number,
+    text: string,
+    completed: boolean
+}
+
+export interface UpdatedTodo {
+    id: number,
+    text: string
+}
