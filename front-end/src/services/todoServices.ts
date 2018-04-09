2ac4929f0695e94661d09a66df216bdfd532dd6a
martinyounghoonkim <hoons0131@gmail.com>
2018-04-09 22:26:55 +0900

    Add todoService's http methods

--- front-end/src/services/todoServices.ts
+++ front-end/src/services/todoServices.ts
@@ -1,16 +1,27 @@
 import { API } from './coreServices';
+import { AddedTodo, UpdatedTodo } from '../types/Todo';
 
 export function fetchTodo(): Promise<object> {
     return API.get('/api/todos');
 }
 
-export function createTodo (todo: string): Promise<object> {
-    return API.post('/api/todos', { todo });
+export function createTodo (payload: AddedTodo): Promise<object> {
+    return API.post('/api/todos', payload);
+}
+
+export function deleteTodo (primaryKey: number) : Promise<object> {
+    return API.delete(`/api/todos/${primaryKey}`);
+}
+
+export function updateTodo (payload: UpdatedTodo): Promise<object> {
+    return API.put('/api/todos', payload);
 }
 
 export default {
     fetchTodo,
-    createTodo
+    createTodo,
+    deleteTodo,
+    updateTodo
 }
 // vo
 // dao => db에서 꺼내왔을 때의 Object -> db에 붙어있는
