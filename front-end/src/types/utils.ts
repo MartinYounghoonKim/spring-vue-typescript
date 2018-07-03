f89a4f2137ea7458a9be5a49e8a608e81f5438a6
martinyounghoonkim <hoons0131@gmail.com>
2018-07-03 23:28:09 +0900

    Add Custom axios response interface

--- front-end/src/types/utils.ts
+++ front-end/src/types/utils.ts
@@ -3,3 +3,10 @@ export interface IHTTPResponse {
     status: number,
     data: object
 };
+
+
+export interface IAxiosResponse<T = any> {
+    data: T;
+    status: number;
+    message?: string;
+}
