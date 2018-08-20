c26457e5b0b24fa76e674dcdffa3ada0576d7f9e
martinyounghoonkim <hoons0131@gmail.com>
2018-08-21 01:04:38 +0900

    Add sample button services file

---
+++ front-end/src/services/buttonServices.ts
@@ -0,0 +1,32 @@
+import apiServices from './coreServices';
+import {Todo} from '../types/Todo';
+import App from '../main';
+
+export function activeButton(): Promise<Todo[]> {
+  return apiServices.getMethod<Todo[]>('/api/todos')
+    .then(res => {
+      if (res.isSuccess) {
+        App.$snotify.info('Hello, anonymous. Welcome to Todo app');
+        return res.data;
+      } else {
+        return [];
+      }
+    });
+}
+
+export function unactiveButton(text: string): Promise<Todo> {
+  return apiServices.postMethod<Todo>('/api/todos', {text})
+    .then(res => {
+      if (res.isSuccess) {
+        return Promise.resolve(res.data);
+      } else {
+        // 서버에서 message 가 내려올 경우
+        return Promise.reject('Something went wrong.');
+      }
+    })
+}
+
+export default {
+  activeButton,
+  unactiveButton,
+}
